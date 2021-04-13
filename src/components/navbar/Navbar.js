0d3f830dbb81cad9377f646a82496ec6e39c7f9b
unknown <akinolawle@gmail.com>
2021-04-13 17:10:58 +0200

    Added two files

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,30 @@
+import "./Navbar.css"
+import avatar from '../../assets/avatar.svg'
+
+const Navbar = ({ sidebarOpen, openSidebar}) => {
+    return (
+        <nav className="navbar">
+            <div className="nav_icon" onClick={() => openSidebar()}>
+                <i className="fa fa-bars"></i>
+            </div>
+            <div className="navbar__left">
+                <a href="#">Subscribers</a>
+                <a href="#">Video Management</a>
+                <a className="active_link" href="#">Admin</a>
+            </div>
+            <div className="navbar__right">
+                <a href="#">
+                    <i className="fa fa-search"></i>
+                </a>
+                <a href="#">
+                    <i className="fa fa-clock-o"></i>
+                </a>
+                <a href="#">
+                    <img width="30" src={avatar} alt="avatar" />
+                </a>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
